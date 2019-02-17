b142275011619efdaa83699f86ddabe166bea498
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-02-17 20:39:48 +0100

    dodaje notatki do local storage

--- javascript.js
+++ javascript.js
@@ -1,12 +1,9 @@
-document.addEventListener('DOMContentLoaded', updateLocalStorage)
+
+//initialization
+document.addEventListener('DOMContentLoaded', addNote2) 
 
 
-
-
-
-
-
-class form {}
+class form  {};
 // add button and areatext to js
 form.noteText = document.querySelector(`#formNoteText1`);
 form.noteText2 = document.querySelector(`#formNoteText2`);
@@ -21,22 +18,20 @@ form.noteText2.focus();
 
 // add new note
 function addNote() {
+  let br = "<br/>"
   let text = form.noteText.value;
   let text2 = form.noteText2.value;
+  let All=text+br+text2;
+  let temp1 =localStorage.getItem('content')+br+br+All;
   let note = document.createElement(`div`);
-  let deleteButton = document.createElement(`span`);
+ 
 
   note.classList.add(`note`);
+ //Add note to form
+  note.innerHTML = `<br/><br/><div class='note-text'>${text}</div><div class='note-text'>${text2}</div>`;
   
-  note.innerHTML = `<div class='note-text'>${text}</div> <div class='note-text'>${text2}</div>`;
-  // delate note
-  deleteButton.classList.add(`note-delete`);
-  deleteButton.innerHTML = `&times;`;
-
-  updateLocalStorage()
-
-
-  note.appendChild(deleteButton);  
+//save value to local storage
+  localStorage.setItem('content',temp1);
   notes.appendChild(note);
 
   // clear areatext after press button
@@ -44,26 +39,38 @@ function addNote() {
   form.noteText.focus();
   form.noteText2.value = ``;
   form.noteText2.focus();
-// adding delate button
-  addListenerDeleteButton(deleteButton);
+
+ 
 }
 
 
-function updateLocalStorage(){
-    localStorage.setItem('notes', JSON.stringify(notes))
-}
 
-function addListenerDeleteButton(deleteButton) {
-  deleteButton.addEventListener(`click`, function (e) {
-    e.stopPropagation();      
-    deleteNote(e);
-  });
-}
+//funcion get value from local storage and diplay on the form 
 
-function deleteNote(e) {
-  let eventNote = e.target.parentNode;
-  eventNote.parentNode.removeChild(eventNote);
-}
+function addNote2() {
+//get value from local storage
+    let text = localStorage.getItem('content');
+    let note = document.createElement(`div`);
+   
+  
+    note.classList.add(`note`);
+   //display on the form 
+    note.innerHTML = `<div class='note-text'>${text}</div>`;
+    notes.appendChild(note);
+  
+    // clear areatext after press button
+    form.noteText.value = ``;
+    form.noteText.focus();
+    form.noteText2.value = ``;
+    form.noteText2.focus();
+  
+
+
+
+
+
+
+
 
 
 
@@ -72,6 +79,9 @@ form.addButton.addEventListener(`click`, function (e) {
   e.preventDefault();  
   if (form.noteText.value != `` && form.noteText2.value != ``) {
     addNote();
+    
+   
+
   }
 })
-
+}
