8cfb9c08f82794960b8e5e24e397206cbbf41760
piotrbarc <43997971+piotrbarc@users.noreply.github.com>
2019-02-17 22:40:17 +0100

    Dodanie daty / aktualizacja styli

--- javascript.js
+++ javascript.js
@@ -18,17 +18,32 @@ form.noteText2.focus();
 
 // add new note
 function addNote() {
-  let br = "<br/>"
-  let text = form.noteText.value;
+
+// get date (yy/mm/dd)
+let d = new Date();
+const date = d.getFullYear();
+const date1 = d.getMonth();
+const date2 = d.getDate();
+// set date as one variable
+const a1 = '<font size="-1">'+date+`.`+date1+`.`+date2+'</font>';
+
+
+
+  
+ 
+  let text = '<font size="+2">'+form.noteText.value+'</font>';
   let text2 = form.noteText2.value;
-  let All=text+br+text2;
-  let temp1 =localStorage.getItem('content')+br+br+All;
+  let All="<strong>" +text + "</strong>"+'<br/>'+text2+'<br/>'+a1;
+  let temp1 =localStorage.getItem('content')+'<br/>'+'<br/>'+All;
   let note = document.createElement(`div`);
  
+  
+  
+
 
   note.classList.add(`note`);
  //Add note to form
-  note.innerHTML = `<br/><br/><div class='note-text'>${text}</div><div class='note-text'>${text2}</div>`;
+  note.innerHTML = `<br/><br/><div class='note-text'><strong>${text}</strong></div><div class='note-text'>${text2}</div>${a1}`;
   
 //save value to local storage
   localStorage.setItem('content',temp1);
@@ -51,11 +66,11 @@ function addNote2() {
 //get value from local storage
     let text = localStorage.getItem('content');
     let note = document.createElement(`div`);
-   
+    
   
     note.classList.add(`note`);
    //display on the form 
-    note.innerHTML = `<div class='note-text'>${text}</div>`;
+    note.innerHTML = `<div class='note-text1'>${text}</div>`;
     notes.appendChild(note);
   
     // clear areatext after press button
